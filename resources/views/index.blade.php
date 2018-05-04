39eacbcc550867e85363e729ff054fc150d4d0d5
Zhangling <lizhangling@heycb.com>
2018-05-04 18:43:22 +0800

    12

---
+++ resources/views/index.blade.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>全国第一视频聚合平台</title>
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">全国第一视频聚合平台</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">首页 <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">视频库</a></li>
+                <li><a href="#">原创视频</a></li>
+                <li><a href="#">意见反馈</a></li>
+                <li><a href="#">关于本站</a></li>
+            </ul>
+            <form class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="输入你想学习的内容">
+                </div>
+                <button type="submit" class="btn btn-default">查找</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">登录/注册</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="margin-top: 70px;">
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="/images/1.jpg" alt="...">
+            <div class="carousel-caption">
+                这是一个非常好的视频
+            </div>
+        </div>
+        <div class="item">
+            <img src="/images/2.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="/images/3.jpg" height="750" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+    </div>
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-md-12"><h2 style="padding: 40px 0;">课程体系</h2></div>
+    </div>
+    <div class="row" style="padding-bottom: 20px">
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>IT/互联网</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>金融</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>法律</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>建筑</h3>
+        </div>
+    </div>
+    <div class="row" style="padding-bottom: 20px">
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>音乐</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>书法</h3>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid text-center" style="background-color: #f3f6fb">
+    <div class="row">
+        <div class="col-md-12"><h2 style="padding: 40px 0;">原创视频</h2></div>
+    </div>
+    <div class="row" style="padding-bottom: 20px">
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>HTML</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>PHP</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>Mysql</h3>
+        </div>
+        <div class="col-md-3">
+            <a href=""><img src="https://res.tuwan.com/templet/teach/newindex/images/lol.jpg?v=1" width="80%"></a>
+            <h3>JavaScript</h3>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-md-12"><h2 style="padding: 40px 0;">精彩留言</h2></div>
+    </div>
+    <table class="table">
+        <tr>
+            <td>用户</td>
+            <td>采纳的留言</td>
+            <td>奖励</td>
+            <td>时间</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+        <tr>
+            <td>小李</td>
+            <td>涉及到付款举案说法</td>
+            <td>奖励《php课程一套》</td>
+            <td>2018-2-3</td>
+        </tr>
+    </table>
+</div>
+
+<div class="container-fluid text-center" style="padding: 80px 0; background-color: #f3f6fb">
+    <a class="btn btn-primary btn-lg">去找资料</a>
+</div>
+
+<div class="jumbotron" style="background-color: #000;margin-bottom: 0;">
+    <h1>版权jelly所有</h1>
+</div>
+</body>
+</html>
